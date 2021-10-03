e0526ca2956852bf97e81917fa1142f4c7ef74a0
Alain Picard <devcoder0x0@gmail.com>
2021-10-03 16:42:21 -0700

    Remove solutions from Vehicle class

--- src/com/alain/inheritance/Vehicle.java
+++ src/com/alain/inheritance/Vehicle.java
@@ -1,23 +1,17 @@
 package com.alain.inheritance;
 
+
+
 public class Vehicle {
     private int topSpeed;
-    private int currentSpeed = 0;
-    private String color;
-    private String make;
-    private String model;
-    private boolean isElectric;
+    //TODO  Add other instance variables here
 
-    public Vehicle(int topSpeed, String color, String make, String model, boolean isElectric) {
-        this.topSpeed = topSpeed;
-        this.color = color;
-        this.make = make;
-        this.model = model;
-        this.isElectric = isElectric;
-    }
+
+    //TODO Create your constructor here
 
 
 
+    //TODO Create your getters and setters here
     public int getTopSpeed() {
         return topSpeed;
     }
@@ -25,67 +19,20 @@ public class Vehicle {
         this.topSpeed = topSpeed;
     }
 
-    public int getCurrentSpeed() {
-        return currentSpeed;
-    }
-    public void setCurrentSpeed(int currentSpeed) {
-        this.currentSpeed = currentSpeed;
-    }
 
-    public String getColor() {
-        return color;
-    }
-    public void setColor(String color) {
-        this.color = color;
-    }
 
-    public String getMake() {
-        return make;
-    }
-    public void setMake(String make) {
-        this.make = make;
-    }
+    //TODO Create your increaseSpeed() method here
 
-    public String getModel() {
-        return model;
-    }
-    public void setModel(String model) {
-        this.model = model;
-    }
 
-    public boolean getIsElectric() {
-        return isElectric;
-    }
-    public void setIsElectric(boolean isElectric) {
-        this.isElectric = isElectric;
-    }
 
-    public void increaseSpeed() {
-        if(currentSpeed + 10 > topSpeed) {
-            throw new IllegalStateException("Top Speed exceeded");
-        }
-        currentSpeed += 10;
-    }
 
-    public void decreaseSpeed() {
-        if(currentSpeed - 10 < 0) {
-            currentSpeed = 0;
-        }
-        else {
-            currentSpeed -= 10;
-        }
-    }
+    //TODO Create your decreaseSpeed() method here
 
-    @Override
-    public String toString() {
-        return "Vehicle + {" +
-                "topSpeed = " + topSpeed +
-                ", currentSpeed = " + currentSpeed +
-                ", color = '" + color + '\'' +
-                ", make = '" + make + '\'' +
-                ", model = '" + model + '\'' +
-                ", isElectric = " + isElectric +
-                '}';
-    }
+
+
+
+    //TODO Create a toString() method here to print the values of your objects
+
+
 
 }
